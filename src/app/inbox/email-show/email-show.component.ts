0da57cce74cd3e8fbe5aada7443d4e33377a1026
Robson Carvalho <robson01jc@gmail.com>
2022-04-06 21:00:34 -0300

    switchMap operator added

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-show',
   templateUrl: './email-show.component.html',
-  styleUrls: ['./email-show.component.css']
+  styleUrls: ['./email-show.component.css'],
 })
 export class EmailShowComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private emailService: EmailService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap(({ id }) => {
+          return this.emailService.getEmail(id);
+        })
+      )
+      .subscribe((email) => {
+        console.log(email);
+      });
   }
-
 }
