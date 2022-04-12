33bd94ce92acedb64806226663c3dfd7905332d7
Robson Carvalho <robson01jc@gmail.com>
2022-04-11 21:37:23 -0300

    reusable modal component

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { InboxRoutingModule } from './inbox-routing.module';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { EmailCreateComponent } from './email-create/email-create.component';
-import { EmailReplyComponent } from './email-reply/email-reply.component';
 import { EmailIndexComponent } from './email-index/email-index.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
 import { EmailShowComponent } from './email-show/email-show.component';
+import { HomeComponent } from './home/home.component';
+import { InboxRoutingModule } from './inbox-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
-
 
 @NgModule({
   declarations: [
@@ -17,11 +17,9 @@ import { PlaceholderComponent } from './placeholder/placeholder.component';
     EmailReplyComponent,
     EmailIndexComponent,
     EmailShowComponent,
-    PlaceholderComponent
+    PlaceholderComponent,
+    NotFoundComponent,
   ],
-  imports: [
-    CommonModule,
-    InboxRoutingModule
-  ]
+  imports: [CommonModule, InboxRoutingModule, SharedModule],
 })
-export class InboxModule { }
+export class InboxModule {}
